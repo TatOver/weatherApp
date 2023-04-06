fa8be1997857e6c3263274d76864b09d4a4c4208
Пользователь <trollka@list.ru>
2023-04-06 14:38:19 +0300

    add openweathermap

--- weatherApp2/weather/views.py
+++ weatherApp2/weather/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
+    appid = 'f703ca03191f7ce7d08884aeaea4ae0d'
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+
+    city = 'London'
+    res = requests.get(url.format(city))
+    print(res.text)
     return render(request,'weather/index.html')
