b739ad0254fa817ab37211916c5564adb4b2b1ed
Пользователь <trollka@list.ru>
2023-04-09 12:48:58 +0300

    add model city

--- weatherApp2/weather/views.py
+++ weatherApp2/weather/views.py
@@ -6,6 +6,14 @@ def index(request):
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
 
     city = 'London'
-    res = requests.get(url.format(city))
-    print(res.text)
-    return render(request,'weather/index.html')
+    res = requests.get(url.format(city)).json()
+
+    city_info={
+        'city': city,
+        'temp': res['main']['temp'],
+        'icon': res['weather'][0]['icon']
+    }
+
+    context = {'info':city_info}
+
+    return render(request,'weather/index.html', context)
